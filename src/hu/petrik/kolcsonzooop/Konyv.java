492446a86f7914ca1e5134435fbbba382ddd7e37
Budaházi Bence Nándor <bence0630@gmail.com>
2021-10-14 11:43:36 +0200

    Initial commit

---
+++ src/hu/petrik/kolcsonzooop/Konyv.java
@@ -0,0 +1,33 @@
+package hu.petrik.kolcsonzooop;
+
+public class Konyv implements Kolcsonozheto{
+    private String cim, szerzo, isbn;
+
+    public Konyv(String szerzo, String cim, String isbn) {
+        this.szerzo = szerzo;
+        this.cim = cim;
+        this.isbn = isbn;
+    }
+
+    public String getCim() {
+        return cim;
+    }
+
+    public String getSzerzo() {
+        return szerzo;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    @Override
+    public String megjelenitendoNev() {
+        return String.format("%s - %s", this.szerzo, this.cim);
+    }
+
+    @Override
+    public int meddigKolcsonozheto() {
+        return 14;
+    }
+}
